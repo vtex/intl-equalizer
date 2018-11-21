a1cb5346107a8a4698a5c058d86c87cb231389e0
Fernando de Almeida Coelho <nandoacoelho@gmail.com>
2018-11-21 18:28:15 -0200

    Update unit tests

--- src/equalizer.test.js
+++ src/equalizer.test.js
@@ -6,33 +6,32 @@ jest.mock('fs')
 describe('Equalizer', () => {
   it('should return error if doesnt have keys in a file', () => {
     require('fs').__setMockFiles({
-      [`${process.cwd()}/src/__mocks__/emptyLocales/emptyPT.json`]: '',
-      [`${process.cwd()}/src/__mocks__/emptyLocales/emptyEN.js`]: '',
+      [`${process.cwd()}/src/__mocks__/emptyLocales/en.js`]: '',
+      [`${process.cwd()}/src/__mocks__/emptyLocales/pt.json`]: '',
     })
-
-    const result = equalize(
-      ['en', 'pt'],
-      `${process.cwd()}/src/__mocks__/emptyLocales`,
-      'en'
-    )
+    const result = equalize({
+      languages: ['en', 'pt'],
+      localesDirectory: `${process.cwd()}/src/__mocks__/emptyLocales`,
+      referenceLocale: 'en',
+    })
 
     expect(result.data).toEqual('en')
     expect(result.error).toEqual(ERRORS.ERROR_NO_KEYS_LOCALE)
   })
 
-  it('should return missing keys', () => {
+  it('should return one missing key', () => {
     require('fs').__setMockFiles({
       [`${process.cwd()}/src/__mocks__/locales/pt.json`]: '',
       [`${process.cwd()}/src/__mocks__/locales/en.js`]: '',
     })
 
-    const result = equalize(
-      ['en', 'pt'],
-      `${process.cwd()}/src/__mocks__/locales`,
-      'pt'
-    )
+    const result = equalize({
+      languages: ['en', 'pt'],
+      localesDirectory: `${process.cwd()}/src/__mocks__/locales`,
+      referenceLocale: 'pt',
+    })
 
-    expect(result['en'].missingKeys.length).toBeGreaterThan(0)
+    expect(result['en'].missingKeys.length).toEqual(1)
   })
 
   it('should return equal locales', () => {
@@ -41,11 +40,11 @@ describe('Equalizer', () => {
       [`${process.cwd()}/src/__mocks__/equalLocales/en.js`]: '',
     })
 
-    const result = equalize(
-      ['en', 'pt'],
-      `${process.cwd()}/src/__mocks__/equalLocales`,
-      'pt'
-    )
+    const result = equalize({
+      languages: ['en', 'pt'],
+      localesDirectory: `${process.cwd()}/src/__mocks__/equalLocales`,
+      referenceLocale: 'pt',
+    })
 
     expect(result['en'].missingKeys.length).toEqual(0)
     expect(result['pt'].missingKeys.length).toEqual(0)
