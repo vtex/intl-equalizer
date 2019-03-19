99d057e741c6b2b050bffb0344254a07d0351198
Thiago Murakami <thiago.murakami@gmail.com>
2019-03-19 16:57:31 -0300

    Add '-all' options flag and handle it on src/start.js.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import commander from 'commander'
 import start from './start'
 import fix from './fix'
 
-commander.option('-f, --fix').parse(process.argv)
+commander.option('-f, --fix')
+         .option('-a, --all', 'Show all errors.')
+         .parse(process.argv)
 
 if (commander.fix) {
   fix()
+}
+
+if (commander.all) {
+  start({all: true})
 }
 
 if (commander.args.length < 1) {
